79f3a18683742c2e67ccd531f10535f6332663cb
mike <miguel.siqueiros17@tectijuana.edu.mx>
2022-06-03 13:50:02 -0700

    Create main page, upload ct-scan image and make prediction

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Button from "@mui/material/Button";
+import { Alert, AppBar, Box, Card, CardActions, CardContent, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Stack, Toolbar, Typography } from '@mui/material';
+import * as tf from "@tensorflow/tfjs";
+import { GraphModel } from '@tensorflow/tfjs';
+import Dropzone from 'react-dropzone';
 
-function App() {
+const modelUrl = "https://storage.googleapis.com/covid19-ct-scan-models/model.json";
+
+const App = () => {
+  const [image, setImage] = useState("");
+  const [cnnModel, setCnnModel] = useState<GraphModel>();
+  const [loadModel, setLoadModel] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [hasCovid, setHasCovid] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  useEffect(() => {
+    tf.ready().then(() => {
+      tf.loadGraphModel(modelUrl).then(model => {
+        setCnnModel(model)
+        setLoadModel(true);
+        console.log("Successfully loaded cnn model");
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Stack spacing={10}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar>
+            <Toolbar>
+              <Typography>COVID-19 diagnosis</Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Grid container justifyContent="center">
+          <Card elevation={8} sx={{ minWidth: 400 }}>
+            <CardContent>
+              {loadModel ? <Grid container direction={"column"} alignItems="center">
+                <Stack spacing={10}>
+                  <Typography variant='h5' gutterBottom component="div">COVID-19 CT-SCAN Classifier</Typography>
+                </Stack>
+                <Dropzone onDrop={acceptedFiles => {
+                  setImage(URL.createObjectURL(acceptedFiles[0]))
+                }}>
+                  {({ getRootProps, getInputProps }) => (
+                    <section>
+                      <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <p>Drag 'n' drop image, or click to select image</p>
+                        {image && <img id='ct-scan-image' width={256} height={256} src={image} alt="image" />}
+                      </div>
+                    </section>
+                  )}
+                </Dropzone>
+              </Grid> : <CircularProgress />}
+            </CardContent>
+            <CardActions>
+              <Grid container direction={"column"} alignItems="center">
+                <Button disabled={image ? false : true} style={{ marginLeft: 10 }} size='small' variant="contained" onClick={() => {
+                  const ctScan = document.getElementById("ct-scan-image") as HTMLImageElement;
+                  const img = tf.browser.fromPixels(ctScan).div(255).mean(2).toFloat().expandDims(0).expandDims(-1);
+                  const prediction = cnnModel?.predict(img) as tf.Tensor<tf.Rank>;
+                  prediction.data().then(data => {
+                    if (data[0] > 0) {
+                      setHasCovid(false);
+                    } else {
+                      setHasCovid(true);
+                    }
+                    handleClickOpen();
+                  });
+                }}>Predict</Button>
+              </Grid>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
         >
-          Learn React
-        </a>
-      </header>
+          <DialogTitle id="alert-dialog-title">
+            {"Diagnostic results"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {hasCovid ? <Alert severity="error">Patient has COVID-19, please take take the necessary precautions and visit a doctor.</Alert> : <Alert severity="info">Patient is Healthy.</Alert>}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Ok</Button>
+          </DialogActions>
+        </Dialog>
+      </Stack>
     </div>
   );
 }
 
 export default App;
+
